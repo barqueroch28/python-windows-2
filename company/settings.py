b59fa73873c6518ff6765ea382c97ea8244d4d08
barqueroch28 <92421341+barqueroch28@users.noreply.github.com>
2023-05-11 13:51:11 -0600

    Update settings.py

--- company/settings.py
+++ company/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = '=kpli-kjn@mk+-^ng+d%-frxc@c-_qr^i_6(+)m+ytm!11ue6@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 
